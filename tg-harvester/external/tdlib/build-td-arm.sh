35f46fda0e0d6c8fd78d2cbc9ce7ea08389a4c25
ymy <icheeringsoul@gmail.com>
2025-08-27 00:40:06 +0800

    add datasource

--- tg-harvester/external/tdlib/build-td-arm.sh
+++ tg-harvester/external/tdlib/build-td-arm.sh
@@ -3,9 +3,9 @@ cd external/tdlib
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES=arm64 \
-  -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl@3 \
-  -DOPENSSL_LIBRARIES=/usr/local/opt/openssl@3/lib \
-  -DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl@3/include \
+  -DOPENSSL_ROOT_DIR=/opt/homebrew/opt/openssl@3 \
+  -DOPENSSL_LIBRARIES=/opt/homebrew/opt/openssl@3/lib \
+  -DOPENSSL_INCLUDE_DIR=/opt/homebrew/opt/openssl@3/include \
   -DJAVA_HOME=/Users/ymy/Library/Java/JavaVirtualMachines/corretto-21.0.6/Contents/Home/ \
   -DCMAKE_INSTALL_PREFIX:PATH=../example/java/td -DTD_ENABLE_JNI=ON ..
 cmake --build . --target install
